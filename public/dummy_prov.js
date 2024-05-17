32a15fc3dc2916f8bc0337b5d2849ab3532059f3
Muhammad Rifaldi Judri <muhamadrifaldi145@gmail.com>
2024-05-18 00:30:51 +0700

    Update

---
+++ public/dummy_prov.js
@@ -0,0 +1,663 @@
+const provinces = [
+  {
+    "name": "Aceh",
+    "latitude": 4.695135,
+    "longitude": 96.749397,
+    "agents": [
+      {
+        "agent_name": "Agent Aceh 1",
+        "users": [
+          {
+            "name": "User A1",
+            "age": 25,
+            "birthdate": "1999-03-15"
+          },
+          {
+            "name": "User A2",
+            "age": 30,
+            "birthdate": "1994-07-22"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Sumatera Utara",
+    "latitude": 2.115354,
+    "longitude": 99.545097,
+    "agents": [
+      {
+        "agent_name": "Agent Sumut 1",
+        "users": [
+          {
+            "name": "User SU1",
+            "age": 35,
+            "birthdate": "1988-05-09"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Sumatera Barat",
+    "latitude": -0.789275,
+    "longitude": 100.663296,
+    "agents": [
+      {
+        "agent_name": "Agent Sumbar 1",
+        "users": [
+          {
+            "name": "User SB1",
+            "age": 27,
+            "birthdate": "1997-08-18"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Riau",
+    "latitude": 0.293347,
+    "longitude": 101.706829,
+    "agents": [
+      {
+        "agent_name": "Agent Riau 1",
+        "users": [
+          {
+            "name": "User R1",
+            "age": 22,
+            "birthdate": "2002-04-25"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Jambi",
+    "latitude": -1.485152,
+    "longitude": 102.438057,
+    "agents": [
+      {
+        "agent_name": "Agent Jambi 1",
+        "users": [
+          {
+            "name": "User J1",
+            "age": 28,
+            "birthdate": "1996-01-11"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Sumatera Selatan",
+    "latitude": -3.319437,
+    "longitude": 104.914551,
+    "agents": [
+      {
+        "agent_name": "Agent Sumsel 1",
+        "users": [
+          {
+            "name": "User SS1",
+            "age": 40,
+            "birthdate": "1983-12-02"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Bengkulu",
+    "latitude": -3.800442,
+    "longitude": 102.265541,
+    "agents": [
+      {
+        "agent_name": "Agent Bengkulu 1",
+        "users": [
+          {
+            "name": "User B1",
+            "age": 29,
+            "birthdate": "1995-06-23"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Lampung",
+    "latitude": -4.558584,
+    "longitude": 105.406807,
+    "agents": [
+      {
+        "agent_name": "Agent Lampung 1",
+        "users": [
+          {
+            "name": "User L1",
+            "age": 32,
+            "birthdate": "1991-09-17"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Kepulauan Bangka Belitung",
+    "latitude": -2.741051,
+    "longitude": 106.440582,
+    "agents": [
+      {
+        "agent_name": "Agent Babel 1",
+        "users": [
+          {
+            "name": "User KBB1",
+            "age": 26,
+            "birthdate": "1998-05-20"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Kepulauan Riau",
+    "latitude": 3.945651,
+    "longitude": 108.142868,
+    "agents": [
+      {
+        "agent_name": "Agent Kepri 1",
+        "users": [
+          {
+            "name": "User KR1",
+            "age": 31,
+            "birthdate": "1993-11-03"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "DKI Jakarta",
+    "latitude": -6.208763,
+    "longitude": 106.845599,
+    "agents": [
+      {
+        "agent_name": "Agent Jakarta 1",
+        "users": [
+          {
+            "name": "User JKT1",
+            "age": 45,
+            "birthdate": "1979-08-30"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Jawa Barat",
+    "latitude": -6.917464,
+    "longitude": 107.619123,
+    "agents": [
+      {
+        "agent_name": "Agent Jabar 1",
+        "users": [
+          {
+            "name": "User JB1",
+            "age": 36,
+            "birthdate": "1987-03-12"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Jawa Tengah",
+    "latitude": -7.150975,
+    "longitude": 110.140259,
+    "agents": [
+      {
+        "agent_name": "Agent Jateng 1",
+        "users": [
+          {
+            "name": "User JT1",
+            "age": 33,
+            "birthdate": "1990-07-14"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "DI Yogyakarta",
+    "latitude": -7.79558,
+    "longitude": 110.369492,
+    "agents": [
+      {
+        "agent_name": "Agent Jogja 1",
+        "users": [
+          {
+            "name": "User DJ1",
+            "age": 27,
+            "birthdate": "1997-02-19"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Jawa Timur",
+    "latitude": -7.250445,
+    "longitude": 112.768845,
+    "agents": [
+      {
+        "agent_name": "Agent Jatim 1",
+        "users": [
+          {
+            "name": "User JTM1",
+            "age": 29,
+            "birthdate": "1995-04-22"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Banten",
+    "latitude": -6.405817,
+    "longitude": 106.064018,
+    "agents": [
+      {
+        "agent_name": "Agent Banten 1",
+        "users": [
+          {
+            "name": "User BN1",
+            "age": 34,
+            "birthdate": "1989-12-07"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Bali",
+    "latitude": -8.340539,
+    "longitude": 115.092062,
+    "agents": [
+      {
+        "agent_name": "Agent Bali 1",
+        "users": [
+          {
+            "name": "User BL1",
+            "age": 26,
+            "birthdate": "1998-11-05"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Nusa Tenggara Barat",
+    "latitude": -8.652933,
+    "longitude": 117.361648,
+    "agents": [
+      {
+        "agent_name": "Agent NTB 1",
+        "users": [
+          {
+            "name": "User NTB1",
+            "age": 31,
+            "birthdate": "1993-06-17"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Nusa Tenggara Timur",
+    "latitude": -8.657381,
+    "longitude": 121.07937,
+    "agents": [
+      {
+        "agent_name": "Agent NTT 1",
+        "users": [
+          {
+            "name": "User NTT1",
+            "age": 37,
+            "birthdate": "1987-09-26"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Kalimantan Barat",
+    "latitude": -0.092273,
+    "longitude": 109.344749,
+    "agents": [
+      {
+        "agent_name": "Agent Kalbar 1",
+        "users": [
+          {
+            "name": "User KB1",
+            "age": 28,
+            "birthdate": "1996-11-14"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Kalimantan Tengah",
+    "latitude": -1.681487,
+    "longitude": 113.382355,
+    "agents": [
+      {
+        "agent_name": "Agent Kalteng 1",
+        "users": [
+          {
+            "name": "User KB1",
+            "age": 28,
+            "birthdate": "1996-11-14"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Kalimantan Tengah",
+    "latitude": -1.681487,
+    "longitude": 113.382355,
+    "agents": [
+      {
+        "agent_name": "Agent Kalteng 1",
+        "users": [
+          {
+            "name": "User KB1",
+            "age": 28,
+            "birthdate": "1996-11-14"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Kalimantan Selatan",
+    "latitude": -3.092641,
+    "longitude": 115.283753,
+    "agents": [
+      {
+        "agent_name": "Agent Kalses 1",
+        "users": [
+          {
+            "name": "User KB1",
+            "age": 28,
+            "birthdate": "1996-11-14"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Kalimantan Timur",
+    "latitude": 0.524766,
+    "longitude": 116.828873,
+    "agents": [
+      {
+        "agent_name": "Agent Kaltim 1",
+        "users": [
+          {
+            "name": "User KT1",
+            "age": 33,
+            "birthdate": "1990-02-14"
+          },
+          {
+            "name": "User KT2",
+            "age": 28,
+            "birthdate": "1996-11-19"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Kalimantan Utara",
+    "latitude": 2.82773,
+    "longitude": 117.292038,
+    "agents": [
+      {
+        "agent_name": "Agent Kalut 1",
+        "users": [
+          {
+            "name": "User KU1",
+            "age": 29,
+            "birthdate": "1995-08-07"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Sulawesi Utara",
+    "latitude": 1.557108,
+    "longitude": 124.848236,
+    "agents": [
+      {
+        "agent_name": "Agent Sulut 1",
+        "users": [
+          {
+            "name": "User SUT1",
+            "age": 31,
+            "birthdate": "1993-04-30"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Sulawesi Tengah",
+    "latitude": -1.430679,
+    "longitude": 121.445618,
+    "agents": [
+      {
+        "agent_name": "Agent Sulteng 1",
+        "users": [
+          {
+            "name": "User STG1",
+            "age": 27,
+            "birthdate": "1996-02-18"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Sulawesi Selatan",
+    "latitude": -3.668799,
+    "longitude": 119.974053,
+    "agents": [
+      {
+        "agent_name": "Agent Sulsel 1",
+        "users": [
+          {
+            "name": "User SSL1",
+            "age": 32,
+            "birthdate": "1992-09-23"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Sulawesi Tenggara",
+    "latitude": -4.12988,
+    "longitude": 122.14463,
+    "agents": [
+      {
+        "agent_name": "Agent Sultenggara 1",
+        "users": [
+          {
+            "name": "User STG2",
+            "age": 30,
+            "birthdate": "1994-06-12"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Gorontalo",
+    "latitude": 0.699937,
+    "longitude": 122.446723,
+    "agents": [
+      {
+        "agent_name": "Agent Gorontalo 1",
+        "users": [
+          {
+            "name": "User GOR1",
+            "age": 35,
+            "birthdate": "1989-11-05"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Sulawesi Barat",
+    "latitude": -2.84399,
+    "longitude": 119.232078,
+    "agents": [
+      {
+        "agent_name": "Agent Sulbar 1",
+        "users": [
+          {
+            "name": "User SBR1",
+            "age": 28,
+            "birthdate": "1996-10-10"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Maluku",
+    "latitude": -3.238461,
+    "longitude": 130.145273,
+    "agents": [
+      {
+        "agent_name": "Agent Maluku 1",
+        "users": [
+          {
+            "name": "User MLK1",
+            "age": 36,
+            "birthdate": "1987-07-28"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Maluku Utara",
+    "latitude": 1.732048,
+    "longitude": 127.638229,
+    "agents": [
+      {
+        "agent_name": "Agent Malut 1",
+        "users": [
+          {
+            "name": "User MLU1",
+            "age": 29,
+            "birthdate": "1995-04-02"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Papua Barat",
+    "latitude": -1.336115,
+    "longitude": 133.174716,
+    "agents": [
+      {
+        "agent_name": "Agent Papbar 1",
+        "users": [
+          {
+            "name": "User PB1",
+            "age": 32,
+            "birthdate": "1992-11-18"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Papua",
+    "latitude": -4.269928,
+    "longitude": 138.080353,
+    "agents": [
+      {
+        "agent_name": "Agent Papua 1",
+        "users": [
+          {
+            "name": "User P1",
+            "age": 27,
+            "birthdate": "1997-02-28"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Papua Selatan",
+    "latitude": -6.294056,
+    "longitude": 140.024859,
+    "agents": [
+      {
+        "agent_name": "Agent Papuasel 1",
+        "users": [
+          {
+            "name": "User PSL1",
+            "age": 30,
+            "birthdate": "1994-09-15"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Papua Tengah",
+    "latitude": -4.496274,
+    "longitude": 136.88726,
+    "agents": [
+      {
+        "agent_name": "Agent Papuateng 1",
+        "users": [
+          {
+            "name": "User PTT1",
+            "age": 33,
+            "birthdate": "1990-05-20"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Papua Pegunungan",
+    "latitude": -3.738058,
+    "longitude": 138.730658,
+    "agents": [
+      {
+        "agent_name": "Agent Papuapeg 1",
+        "users": [
+          {
+            "name": "User PPG1",
+            "age": 29,
+            "birthdate": "1995",
+            "name": "User PPG1",
+            "age": 29,
+            "birthdate": "1995-03-10"
+          }
+        ]
+      }
+    ]
+  }
+];
+
+module.exports = provinces;
